b6b09b83b09baa0c591f554e650d01ac5ff2515c
makowskid <makowskid@users.noreply.github.com>
2024-11-04 10:31:49 +0000

    Updated latest blog posts

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'sharpapi/sharpapi.github.io',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'a4ef9d1a1ff39fb0eb9494fbb75d7b9b3b598257',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'dg/rss-php' => array(
+            'pretty_version' => 'v1.5',
+            'version' => '1.5.0.0',
+            'reference' => '18f00ab1828948a8cfe107729ca1f11c20129b47',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../dg/rss-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'sharpapi/sharpapi.github.io' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'a4ef9d1a1ff39fb0eb9494fbb75d7b9b3b598257',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
